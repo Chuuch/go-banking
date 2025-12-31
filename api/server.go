e7ba08db1132b4ba89b38bb2f45802bf1117dd34
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2025-12-31 12:59:01 +0200

    Add: Register custom currency validator
    We call gin's binding to access the validator engine and register our
    custom validCurrency helper variable.

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/chuuch/go-banking/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,6 +16,12 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	// We call gin's binding to access the validator engine and register
+	// our custom validCurrency helper variable
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	// Create account
 	router.POST("/accounts", server.createAccount)
