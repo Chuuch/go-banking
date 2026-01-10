48f4f346d8d9cf34b499e72d3357137f32e058c0
Daniel Chuchulev <78451418+Chuuch@users.noreply.github.com>
2026-01-10 14:12:33 +0200

    Ft/session (#6)
    * ft/session: add refresh token duration to our server config struct
    
    * ft/session: create session sql query for sqlc
    
    * ft/session: Go code for our session logic generated from SQLC
    
    * ft/session: gomock generated code for session test mocks
    
    * ft/session: return an additional payload param from our paseto token maker
    
    * ft/session: renew access token logic for our session implementing the refresh payload
    
    * ft/session: create access & refresh token & paylaod from our token maker in our user API.
    
    * ft/session: user API unit testing the login api
    
    * ft/session: add the renew access route to our server
    
    * ft/session: create the payload in our middleware from the token maker and verify its not empty
    
    * ft/session: improve for loop syntax
    
    * add: fix makefile in ft/session branch

---
+++ api/token.go
@@ -0,0 +1,90 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type renewAccessTokenRequest struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
+
+type renewAccessTokenResponse struct {
+	AccessToken          string    `json:"access_token"`
+	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+}
+
+func (server *Server) renewAccessToken(ctx *gin.Context) {
+	var req renewAccessTokenRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	refreshPayload, err := server.tokenMaker.VerifyToken(req.RefreshToken)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	// We get the user from the database passing the context and username params
+	session, err := server.store.GetSession(ctx, refreshPayload.ID)
+	if err != nil {
+		// If there is no user found we return an SQL error no rows
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		// if other we just return a internal server error
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	if session.IsBlocked {
+		err := fmt.Errorf("blocked session")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	if session.Username != refreshPayload.Username {
+		err := fmt.Errorf("incorrect session user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	if session.RefreshToken != req.RefreshToken {
+		err := fmt.Errorf("mismatched session token")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	if time.Now().After(session.ExpiresAt) {
+		err := fmt.Errorf("expired session")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		refreshPayload.Username,
+		server.config.AccessTokenDuration,
+	)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// finally we create a response object with our loginUserResponse struct
+	// and pass the accessToken and our new user response
+	// not containing sensitive info e.g. hashed password
+	rsp := renewAccessTokenResponse{
+		AccessToken:          accessToken,
+		AccessTokenExpiresAt: accessPayload.ExpiredAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}
