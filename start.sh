92d29e8e1232a0dad030f5a05da7311d04dea5c0
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-01-11 17:53:46 +0200

    ft/migrations: remove migrations related scripts from our start script

--- start.sh
+++ start.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
 set -e
-# Only source the file if it exists (prevents the 'not found' error)
-if [ -f "/app/app.env" ]; then
-    echo "loading constants from /app/app.env"
-    . /app/app.env
-fi
 
-# Run migrations with our DB_SOURCE flag
-echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
 
 echo "start the app"
 exec "$@"
