9b00dc5f00698e762b56c5e5b59054985a37087b
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-01-15 21:46:24 +0200

    ft/worker: initial setup

---
+++ worker/distributor.go
@@ -0,0 +1,14 @@
+package worker
+
+import (
+	"github.com/hibiken/asynq"
+)
+
+type TaskDistributor interface {
+}
+
+type RedisTaskDistributor struct {
+	client *asynq.Client
+}
+
+func NewRedisTaskDistributor(redisOpt asynq.RedisClientOpt) TaskDistributor {}
