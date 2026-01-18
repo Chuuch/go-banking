c5dca02d77c042fa08612eb13233c1242748bce1
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-01-18 18:55:33 +0200

    refactor(config): remove unused DB_DRIVER field from configuration

--- util/config.go
+++ util/config.go
@@ -10,7 +10,6 @@ import (
 // The values are read from viper from a config file or environment variables.
 type Config struct {
 	Environment          string        `mapstructure:"ENVIRONMENT"`
-	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
 	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
