01dd4943c0dd6b4335ab5942b5a3d3a9f4c5b6ff
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-01-18 18:59:17 +0200

    refactor(tests): update main_test to use pgxpool and improve testStore initialization

---
+++ gapi/main_test.go
@@ -0,0 +1,39 @@
+package gapi
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	db "github.com/chuuch/go-banking/db/sqlc"
+	"github.com/chuuch/go-banking/token"
+	"github.com/chuuch/go-banking/util"
+	"github.com/chuuch/go-banking/worker"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
+)
+
+func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store, taskDistributor)
+	require.NoError(t, err)
+
+	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
+}
