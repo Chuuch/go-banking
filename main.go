fa2f7dfe1d84008fd51c6b12d8becf45a1645907
Daniel Chuchulev <78451418+Chuuch@users.noreply.github.com>
2026-01-24 10:28:53 +0200

    Ft/cors (#14)
    * feat(main): implement graceful shutdown for servers and enhance context handling in main application
    
    * feat(issuer): add DNS01 solver configuration for Route53 and update ingress-grpc annotations for ACME HTTP01 challenge
    
    * fix(main): handle server shutdown gracefully by avoiding error logging for stopped servers
    
    * feat(cors): integrate CORS middleware and configure allowed origins from environment variables

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/chuuch/go-banking/mail"
 	"github.com/hibiken/asynq"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/cors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/errgroup"
@@ -190,8 +191,24 @@ func runGatewayServer(ctx context.Context, waitGroup *errgroup.Group, config uti
 	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
 	mux.Handle("/swagger/", swaggerHandler)
 
+	c := cors.New(cors.Options{
+		AllowedOrigins: config.AllowedOrigins,
+		AllowedHeaders: []string{
+			"Authorization",
+			"Content-Type",
+		},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+	})
+
+	handler := c.Handler(gapi.HttpLogger(mux))
 	httpServer := &http.Server{
-		Handler: gapi.HttpLogger(mux),
+		Handler: handler,
 		Addr:    config.HTTPServerAddress,
 	}
 
